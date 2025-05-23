c365c44d914a4bc22f2f55dfa8293415fd8de721
221V <221v92@gmail.com>
2025-05-23 05:51:58 +0300

    router

--- vtest/source/app.d
+++ vtest/source/app.d
@@ -1,22 +1,32 @@
 
 import vibe.vibe;
+import vibe.http.websockets;
 
-void main()
-{
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-	auto listener = listenHTTP(settings, &hello);
-	scope (exit)
-	{
-		listener.stopListening();
-	}
-
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-	runApplication();
+void main(){
+  auto settings = new HTTPServerSettings;
+  settings.port = 8080;
+  settings.bindAddresses = ["::1", "127.0.0.1"];
+  
+  auto router = new URLRouter;
+  router.get("/", &index);
+  
+  //auto listener = listenHTTP(settings, &hello);
+  auto listener = listenHTTP(settings, router);
+  scope (exit){
+    listener.stopListening();
+  }
+  
+  logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+  runApplication();
 }
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.writeBody("Hello, World!");
+void index(HTTPServerRequest req, HTTPServerResponse res){
+  res.writeBody("Hello, World!");
 }
+
+/*
+void hello(HTTPServerRequest req, HTTPServerResponse res){
+  res.writeBody("Hello, World!");
+}
+*/
+
