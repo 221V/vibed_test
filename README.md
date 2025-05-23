e12391a3866e01e9c3c6d9671ed6331bd2829ff9
221V <221v92@gmail.com>
2025-05-23 07:28:13 +0300

    ws echo

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ $ cd <FOLDER_NAME>
 $ make c
 
 
+http://127.0.0.1:8080/index.html
+
+
 $ ldc2 -v
 binary    /home/e/.dlang/bin/ldc2
 version   1.39.0 (DMD v2.109.1, LLVM 18.1.6)
--- vtest/source/app.d
+++ vtest/source/app.d
@@ -1,14 +1,30 @@
 
-import vibe.vibe;
+//import vibe.vibe;
+// https://github.com/vibe-d/vibe.d/blob/master/source/vibe/vibe.d
+
+import vibe.core.core;
+import vibe.http.router;
+import vibe.http.server;
+import vibe.http.fileserver;
 import vibe.http.websockets;
+import vibe.core.log;
+
 
 void main(){
   auto settings = new HTTPServerSettings;
   settings.port = 8080;
   settings.bindAddresses = ["::1", "127.0.0.1"];
   
+  //auto fsettings = new HTTPFileServerSettings;
+  //fsettings.serverPathPrefix = "/static";
+  
   auto router = new URLRouter;
   router.get("/", &index);
+  ////router.get("static/*", serverStaticFiles("public/", fsettings) );
+  
+  //router.get("/", staticTemplate!"index.html"); // todo fix
+  router.get("/ws", handleWebSockets(&ws_handle) );
+  router.get("*", serveStaticFiles("public/"));
   
   //auto listener = listenHTTP(settings, &hello);
   auto listener = listenHTTP(settings, router);
@@ -20,9 +36,20 @@ void main(){
   runApplication();
 }
 
+
+void ws_handle(scope WebSocket sock){
+  // simple echo server + :)
+  while(sock.connected){
+    auto msg = sock.receiveText();
+    sock.send(msg ~ " :)");
+  }
+}
+
+
 void index(HTTPServerRequest req, HTTPServerResponse res){
   res.writeBody("Hello, World!");
 }
+
 
 /*
 void hello(HTTPServerRequest req, HTTPServerResponse res){
